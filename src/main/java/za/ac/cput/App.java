77d58ea4b52ef70a38c8319c519cdd92f798244c
AJ <abrahamj.wiese@gmail.com>
2019-02-13 23:05:37 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String concatenate(String color1, String color2){
+
+        return color1+color2;
+    }
+}
---
+++ src/test/java/za/ac/cput/AppTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testConcatenate() {
+        App app = new App();
+
+        String result = app.concatenate("lime","green");
+
+        assertEquals("limegreen", result);
+    }
+}
